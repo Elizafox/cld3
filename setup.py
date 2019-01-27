608c3fd99b77b46c7ff4b67dbaa1520abd256193
boris <boris@verhovs.ky>
2019-01-27 14:40:00 -0700

    formatted setup.py with black

--- setup.py
+++ setup.py
@@ -9,44 +9,46 @@ from distutils.command.build import build
 from Cython.Build import cythonize
 
 
-PROTOS = ["src/sentence.proto", "src/feature_extractor.proto",
-          "src/task_spec.proto"]
+PROTOS = ["src/sentence.proto", "src/feature_extractor.proto", "src/task_spec.proto"]
 
-SOURCES = ["src/cld3.pyx",
-           "src/base.cc",
-           "src/cld_3/protos/src/feature_extractor.pb.cc",
-           "src/cld_3/protos/src/sentence.pb.cc",
-           "src/cld_3/protos/src/task_spec.pb.cc",
-           "src/embedding_feature_extractor.cc",
-           "src/embedding_network.cc",
-           "src/feature_extractor.cc",
-           "src/feature_types.cc",
-           "src/fml_parser.cc",
-           "src/lang_id_nn_params.cc",
-           "src/language_identifier_features.cc",
-           "src/nnet_language_identifier.cc",
-           "src/registry.cc",
-           "src/relevant_script_feature.cc",
-           "src/script_span/fixunicodevalue.cc",
-           "src/script_span/generated_entities.cc",
-           "src/script_span/generated_ulscript.cc",
-           "src/script_span/getonescriptspan.cc",
-           "src/script_span/offsetmap.cc",
-           "src/script_span/text_processing.cc",
-           "src/script_span/utf8statetable.cc",
-           "src/sentence_features.cc",
-           "src/task_context.cc",
-           "src/task_context_params.cc",
-           "src/unicodetext.cc",
-           "src/utils.cc",
-           "src/workspace.cc"]
+SOURCES = [
+    "src/cld3.pyx",
+    "src/base.cc",
+    "src/cld_3/protos/src/feature_extractor.pb.cc",
+    "src/cld_3/protos/src/sentence.pb.cc",
+    "src/cld_3/protos/src/task_spec.pb.cc",
+    "src/embedding_feature_extractor.cc",
+    "src/embedding_network.cc",
+    "src/feature_extractor.cc",
+    "src/feature_types.cc",
+    "src/fml_parser.cc",
+    "src/lang_id_nn_params.cc",
+    "src/language_identifier_features.cc",
+    "src/nnet_language_identifier.cc",
+    "src/registry.cc",
+    "src/relevant_script_feature.cc",
+    "src/script_span/fixunicodevalue.cc",
+    "src/script_span/generated_entities.cc",
+    "src/script_span/generated_ulscript.cc",
+    "src/script_span/getonescriptspan.cc",
+    "src/script_span/offsetmap.cc",
+    "src/script_span/text_processing.cc",
+    "src/script_span/utf8statetable.cc",
+    "src/sentence_features.cc",
+    "src/task_context.cc",
+    "src/task_context_params.cc",
+    "src/unicodetext.cc",
+    "src/utils.cc",
+    "src/workspace.cc",
+]
 
 INCLUDES = ["./src", "./src/cld_3/protos/"]
 
 LIBRARIES = ["protobuf"]
 
-LONG_DESCRIPTION = \
-"""Python bindings for the CLD3 language classification library by Google."""
+LONG_DESCRIPTION = (
+    """Python bindings for the CLD3 language classification library by Google."""
+)
 
 CLASSIFIERS = [
     "License :: OSI Approved :: Apache Software License",
@@ -57,7 +59,8 @@ CLASSIFIERS = [
     "Operating System :: OS Independent",
     "Development Status :: 4 - Beta",
     "Topic :: Text Processing :: Linguistic",
-    "Intended Audience :: Developers",]
+    "Intended Audience :: Developers",
+]
 
 
 class BuildProtobuf(build):
@@ -69,8 +72,7 @@ class BuildProtobuf(build):
         # Build protobuf stuff
         command = ["protoc"]
         command.extend(PROTOS)
-        command.append("--cpp_out={}".format(
-            path.join("src", "cld_3", "protos")))
+        command.append("--cpp_out={}".format(path.join("src", "cld_3", "protos")))
         subprocess.run(command, check=True)
 
         build.run(self)
@@ -82,7 +84,8 @@ ext = Extension(
     include_dirs=INCLUDES,
     libraries=LIBRARIES,
     language="c++",
-    extra_compile_args=["-std=c++11"])
+    extra_compile_args=["-std=c++11"],
+)
 
 setup(
     name="cld3",
@@ -95,4 +98,5 @@ setup(
     license="Apache2",
     keywords=["cld3", "cffi"],
     url="https://github.com/Elizafox/cld3",
-    ext_modules=cythonize([ext]))
+    ext_modules=cythonize([ext]),
+)
